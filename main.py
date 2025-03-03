fa5c23773f89d53e31c9c89c23e4863b6fdc5c06
Uk413 <150799767+Uk413@users.noreply.github.com>
2025-03-03 16:04:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ from src.utils import (
     infer_subcategory,
     infer_yes_no,
     generate_drill_description,
-    suggest_event_names,
-    parse_user_choice,
     infer_purpose,
     generate_faq_answers,
     analyze_name_choice,
@@ -23,7 +21,6 @@ from src.utils import (
     handle_name_suggestion,
     infer_eligibility,
     recognize_name_intent,
-    handle_name_selection
 )
 from src.constants import DEFAULT_DRILL_INFO, CATEGORY_SUBCATEGORY_MAP
 import json
